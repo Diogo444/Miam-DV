48c1836fd7bab961f127e1ba37e2a7d2e35ec728
Diogo444 <da7822000@gmail.com>
2025-11-23 12:31:42 +0100

    Ajout pour l'authentifications

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { MenusModule } from './menus/menus.module';
 import { ProverbesModule } from './proverbes/proverbes.module';
 import { AdminModule } from './admin/admin.module';
 import { SuggestionsModule } from './suggestions/suggestions.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [MenusModule, ProverbesModule, AdminModule, SuggestionsModule],
+  imports: [MenusModule, ProverbesModule, AdminModule, SuggestionsModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
